6e8dc36f1afa772e7c5fbe384d914f22631a9027
vanlam1995 <68365282+vanlam1995@users.noreply.github.com>
2021-09-26 20:58:05 +0700

    Update admin_master.blade.php

--- resources/views/layouts/admin_master.blade.php
+++ resources/views/layouts/admin_master.blade.php
@@ -7,9 +7,9 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-	<title>Syndash - @yield('title')</title>
+	<title>BigDeal - @yield('title')</title>
 	<!--favicon-->
-	<link rel="icon" href="{{ URL::to('backend/images/favicon-32x32.png')}}" type="image/png" />
+	<link rel="icon" href="{{ URL::to('backend/images/logo.png')}}" type="image/png" />
 	<!-- Vector CSS -->
 	<link href="{{ URL::to('backend/plugins/vectormap/jquery-jvectormap-2.0.2.css')}}" rel="stylesheet" />
 	<!--plugins-->
@@ -57,12 +57,6 @@
 		<!--end overlay-->
 		<!--Start Back To Top Button--> <a href="javaScript:;" class="back-to-top"><i class='bx bxs-up-arrow-alt'></i></a>
 		<!--End Back To Top Button-->
-		<!--footer -->
-		<div class="footer">
-			<p class="mb-0">Shop D.ark @2020 | Prod By : <a href="https://themeforest.net/user/codervent" target="_blank">ZuiZui</a>
-			</p>
-		</div>
-		<!-- end footer -->
 	</div>
 	<!-- end wrapper -->
 	<!--start switcher-->
