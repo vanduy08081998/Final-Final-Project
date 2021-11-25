d8c1a6ca2b52fa18a9d76221cb810b48e0a33e6d
vanduy08081998 <vanduy08081998@gmail.com>
2021-11-25 16:42:01 +0700

    edit Template with data

--- app/Http/Controllers/Clients/ProductController.php
+++ app/Http/Controllers/Clients/ProductController.php
@@ -2,16 +2,23 @@
 
 namespace App\Http\Controllers\Clients;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Brand;
 use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ProductController extends Controller
 {
     public function shopGrid() {
+        $categories = Category::where('category_parent_id', null)->orderBy('id_cate', 'desc')->get();
         $product = Product::orderByDESC('id')->get();
+        $brands = Brand::orderByDESC('id')->get();
+        // dd($product->where('product_id_category', 24)->count());
         return view('clients.shop.shop-grid-ls', [
             'product' => $product,
+            'category' => $categories,
+            'brands' => $brands,
         ]);
     }
 
