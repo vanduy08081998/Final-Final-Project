1f5541deb44173beb268d64def37c8da619ceb4c
vanduy08081998 <vanduy08081998@gmail.com>
2021-12-01 10:56:19 +0700

    fix product layout

--- resources/views/clients/shop/shop-list-ls.blade.php
+++ resources/views/clients/shop/shop-list-ls.blade.php
@@ -6,7 +6,149 @@
 
 @section('content')
     <!-- Quick View Modal-->
-    @include('clients.Inc.quickview');
+    <div class="modal-quick-view modal fade" id="quick-view" tabindex="-1">
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title product-title"><a href="shop-single-v1.html" data-bs-toggle="tooltip"
+                            data-bs-placement="right" title="Go to product page">Sports Hooded Sweatshirt<i
+                                class="ci-arrow-right fs-lg ms-2"></i></a></h4>
+                    <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <!-- Product gallery-->
+                        <div class="col-lg-7 pe-lg-0">
+                            <div class="product-gallery">
+                                <div class="product-gallery-preview order-sm-2">
+                                    <div class="product-gallery-preview-item active" id="first"><img class="image-zoom"
+                                            src="img/shop/single/gallery/01.jpg" data-zoom="img/shop/single/gallery/01.jpg"
+                                            alt="Product image">
+                                        <div class="image-zoom-pane"></div>
+                                    </div>
+                                    <div class="product-gallery-preview-item" id="second"><img class="image-zoom"
+                                            src="img/shop/single/gallery/02.jpg" data-zoom="img/shop/single/gallery/02.jpg"
+                                            alt="Product image">
+                                        <div class="image-zoom-pane"></div>
+                                    </div>
+                                    <div class="product-gallery-preview-item" id="third"><img class="image-zoom"
+                                            src="img/shop/single/gallery/03.jpg" data-zoom="img/shop/single/gallery/03.jpg"
+                                            alt="Product image">
+                                        <div class="image-zoom-pane"></div>
+                                    </div>
+                                    <div class="product-gallery-preview-item" id="fourth"><img class="image-zoom"
+                                            src="img/shop/single/gallery/04.jpg" data-zoom="img/shop/single/gallery/04.jpg"
+                                            alt="Product image">
+                                        <div class="image-zoom-pane"></div>
+                                    </div>
+                                </div>
+                                <div class="product-gallery-thumblist order-sm-1"><a
+                                        class="product-gallery-thumblist-item active" href="#first"><img
+                                            src="img/shop/single/gallery/th01.jpg" alt="Product thumb"></a><a
+                                        class="product-gallery-thumblist-item" href="#second"><img
+                                            src="img/shop/single/gallery/th02.jpg" alt="Product thumb"></a><a
+                                        class="product-gallery-thumblist-item" href="#third"><img
+                                            src="img/shop/single/gallery/th03.jpg" alt="Product thumb"></a><a
+                                        class="product-gallery-thumblist-item" href="#fourth"><img
+                                            src="img/shop/single/gallery/th04.jpg" alt="Product thumb"></a></div>
+                            </div>
+                        </div>
+                        <!-- Product details-->
+                        <div class="col-lg-5 pt-4 pt-lg-0 image-zoom-pane">
+                            <div class="product-details ms-auto pb-3">
+                                <div class="d-flex justify-content-between align-items-center mb-2"><a
+                                        href="shop-single-v1.html#reviews">
+                                        <div class="star-rating"><i class="star-rating-icon ci-star-filled active"></i><i
+                                                class="star-rating-icon ci-star-filled active"></i><i
+                                                class="star-rating-icon ci-star-filled active"></i><i
+                                                class="star-rating-icon ci-star-filled active"></i><i
+                                                class="star-rating-icon ci-star"></i>
+                                        </div><span class="d-inline-block fs-sm text-body align-middle mt-1 ms-1">74
+                                            Reviews</span>
+                                    </a>
+                                    <button class="btn-wishlist" type="button" data-bs-toggle="tooltip"
+                                        title="Add to wishlist"><i class="ci-heart"></i></button>
+                                </div>
+                                <div class="mb-3"><span
+                                        class="h3 fw-normal text-accent me-1">$18.<small>99</small></span>
+                                    <del class="text-muted fs-lg me-3">$25.<small>00</small></del><span
+                                        class="badge bg-danger badge-shadow align-middle mt-n2">Sale</span>
+                                </div>
+                                <div class="fs-sm mb-4"><span class="text-heading fw-medium me-1">Color:</span><span
+                                        class="text-muted" id="colorOptionText">Red/Dark blue/White</span></div>
+                                <div class="position-relative me-n4 mb-3">
+                                    <div class="form-check form-option form-check-inline mb-2">
+                                        <input class="form-check-input" type="radio" name="color" id="color1"
+                                            data-bs-label="colorOptionText" value="Red/Dark blue/White" checked>
+                                        <label class="form-option-label rounded-circle" for="color1"><span
+                                                class="form-option-color rounded-circle"
+                                                style="background-image: url(img/shop/single/color-opt-1.png)"></span></label>
+                                    </div>
+                                    <div class="form-check form-option form-check-inline mb-2">
+                                        <input class="form-check-input" type="radio" name="color" id="color2"
+                                            data-bs-label="colorOptionText" value="Beige/White/Black">
+                                        <label class="form-option-label rounded-circle" for="color2"><span
+                                                class="form-option-color rounded-circle"
+                                                style="background-image: url(img/shop/single/color-opt-2.png)"></span></label>
+                                    </div>
+                                    <div class="form-check form-option form-check-inline mb-2">
+                                        <input class="form-check-input" type="radio" name="color" id="color3"
+                                            data-bs-label="colorOptionText" value="Dark grey/White/Mustard">
+                                        <label class="form-option-label rounded-circle" for="color3"><span
+                                                class="form-option-color rounded-circle"
+                                                style="background-image: url(img/shop/single/color-opt-3.png)"></span></label>
+                                    </div>
+                                    <div class="product-badge product-available mt-n1"><i
+                                            class="ci-security-check"></i>Product available</div>
+                                </div>
+                                <form class="mb-grid-gutter">
+                                    <div class="mb-3">
+                                        <label class="fw-medium pb-1" for="product-size">Size:</label>
+                                        <select class="form-select" required id="product-size">
+                                            <option value="">Select size</option>
+                                            <option value="xs">XS</option>
+                                            <option value="s">S</option>
+                                            <option value="m">M</option>
+                                            <option value="l">L</option>
+                                            <option value="xl">XL</option>
+                                        </select>
+                                    </div>
+                                    <div class="mb-3 d-flex align-items-center">
+                                        <select class="form-select me-3" style="width: 5rem;">
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                            <option value="4">4</option>
+                                            <option value="5">5</option>
+                                        </select>
+                                        <button class="btn btn-primary btn-shadow d-block w-100" type="submit"><i
+                                                class="ci-cart fs-lg me-2"></i>Add to Cart</button>
+                                    </div>
+                                </form>
+                                <h5 class="h6 mb-3 pb-2 border-bottom"><i
+                                        class="ci-announcement text-muted fs-lg align-middle mt-n1 me-2"></i>Product info
+                                </h5>
+                                <h6 class="fs-sm mb-2">Style</h6>
+                                <ul class="fs-sm ps-4">
+                                    <li>Hooded top</li>
+                                </ul>
+                                <h6 class="fs-sm mb-2">Composition</h6>
+                                <ul class="fs-sm ps-4">
+                                    <li>Elastic rib: Cotton 95%, Elastane 5%</li>
+                                    <li>Lining: Cotton 100%</li>
+                                    <li>Cotton 80%, Polyester 20%</li>
+                                </ul>
+                                <h6 class="fs-sm mb-2">Art. No.</h6>
+                                <ul class="fs-sm ps-4 mb-0">
+                                    <li>183260098</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- Page Title-->
     <div class="page-title-overlap bg-dark pt-4">
         <div class="container py-2 py-lg-3">
@@ -28,7 +170,6 @@
         <div class="row">
             <!-- Sidebar-->
             <aside class="col-lg-3">
-                @include('Clients.shop.navbar-shop');
                 <!-- Sidebar-->
                 <div class="offcanvas offcanvas-collapse bg-white w-100 rounded-3 shadow-lg py-1" id="shop-sidebar"
                     style="max-width: 23rem;">
@@ -874,19 +1015,18 @@
                 </div>
                 <!-- Products list-->
                 <!-- Product-->
-                @foreach ($product as $pro)
                 <div class="card product-card product-list">
-                    <button class="btn-wishlist btn-sm" type="button" data-bs-toggle="tooltip" data-bs-placement="left" title="Yêu thích"><i class="ci-heart"></i></button>
-                    <div class="d-sm-flex align-items-center"><a class="product-list-thumb" href="shop-single-v1.html">
-                        <img src="{{ URL::to($pro->product_image) }}" alt="Product"></a>
-                        <div class="card-body py-2">
-                            <a class="product-meta d-block fs-xs pb-1" href="#">{{ $pro->Category->category_name }}</a>
-                            <h3 class="product-title fs-base">
-                                <a href="shop-single-v1.html">{{ $pro->product_name }}</a>
+                    <button class="btn-wishlist btn-sm" type="button" data-bs-toggle="tooltip" data-bs-placement="left"
+                        title="Add to wishlist"><i class="ci-heart"></i></button>
+                    <div class="d-sm-flex align-items-center"><a class="product-list-thumb"
+                            href="shop-single-v1.html"><img src="{{ asset('frontend/img/shop/catalog/01.jpg') }}"
+                                alt="Product"></a>
+                        <div class="card-body py-2"><a class="product-meta d-block fs-xs pb-1" href="#">Sneakers &amp;
+                                Keds</a>
+                            <h3 class="product-title fs-base"><a href="shop-single-v1.html">Women Colorblock Sneakers</a>
                             </h3>
                             <div class="d-flex justify-content-between">
-                                <div class="product-price">
-                                    <span class="text-accent">{{ number_format($pro->unit_price) }}<small> VNĐ</small></span>
+                                <div class="product-price"><span class="text-accent">$154.<small>00</small></span>
                                 </div>
                                 <div class="star-rating"><i class="star-rating-icon ci-star-filled active"></i><i
                                         class="star-rating-icon ci-star-filled active"></i><i
@@ -914,22 +1054,17 @@
                                         <label class="form-option-label" for="s-90">9</label>
                                     </div>
                                 </div>
-                                <button class="btn btn-primary btn-sm mb-2" type="button">
-                                    <i class="ci-cart fs-sm me-1"></i>Thêm vào giỏ hàng
-                                </button>
-                                <div class="text-start">
-                                    <a class="nav-link-style fs-ms" href="#quick-view" data-bs-toggle="modal">
-                                        <i class="ci-eye align-middle me-1"></i>Xem nhanh
-                                    </a>
-                                </div>
+                                <button class="btn btn-primary btn-sm mb-2" type="button"><i
+                                        class="ci-cart fs-sm me-1"></i>Add to Cart</button>
+                                <div class="text-start"><a class="nav-link-style fs-ms" href="#quick-view"
+                                        data-bs-toggle="modal"><i class="ci-eye align-middle me-1"></i>Quick view</a></div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="border-top pt-3 mt-3"></div>
-                @endforeach
                 <!-- Product-->
-                {{-- <div class="card product-card product-list"><span class="badge bg-danger badge-shadow">Sale</span>
+                <div class="card product-card product-list"><span class="badge bg-danger badge-shadow">Sale</span>
                     <button class="btn-wishlist btn-sm" type="button" data-bs-toggle="tooltip" data-bs-placement="left"
                         title="Add to wishlist"><i class="ci-heart"></i></button>
                     <div class="d-sm-flex align-items-center"><a class="product-list-thumb"
@@ -1432,7 +1567,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="border-top pt-3 mt-3"></div> --}}
+                <div class="border-top pt-3 mt-3"></div>
                 <!-- Pagination-->
                 <nav class="d-flex justify-content-between pt-2" aria-label="Page navigation">
                     <ul class="pagination">
