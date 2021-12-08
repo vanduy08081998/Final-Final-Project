3ef55ed76cdfd808795846bd2a940353ae263b8b
phamnhunhattruong24072001 <76764238+phamnhunhattruong24072001@users.noreply.github.com>
2021-12-08 20:05:21 +0700

    push

--- resources/views/layouts/client_master.blade.php
+++ resources/views/layouts/client_master.blade.php
@@ -5,58 +5,55 @@
 <meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
 
 <head>
-  <meta charset="utf-8">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>BigDeal | @yield('title')</title>
-  <!-- SEO Meta Tags-->
-  @yield('meta')
-  <!-- Viewport-->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- Favicon and Touch Icons-->
-  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('frontend/img/logo/favicon.png') }}">
-  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('frontend/img/logo/favicon.png') }}">
-  {{--
-  <link rel="manifest" href="site.webmanifest"> --}}
-  <link rel="mask-icon" color="#fe6a6a" href="safari-pinned-tab.svg">
-  <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="theme-color" content="#ffffff">
-  <!-- Vendor Styles including: Font Icons, Plugins, etc.-->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-  <link rel="stylesheet" media="screen" href="{{ asset('frontend/vendor/simplebar/dist/simplebar.min.css') }}" />
-  <link rel="stylesheet" media="screen" href="{{ asset('frontend/vendor/tiny-slider/dist/tiny-slider.css') }}" />
-  <link rel="stylesheet" media="screen" href="{{ asset('frontend/vendor/drift-zoom/dist/drift-basic.min.css') }}" />
-  <link rel="stylesheet" media="screen"
-    href="{{ asset('frontend/vendor/lightgallery.js/dist/css/lightgallery.min.css') }}" />
-  <!-- Main Theme Styles + Bootstrap-->
-  <link rel="stylesheet" media="screen" href="{{ asset('frontend/css/theme.min.css') }}">
-  <link rel="stylesheet" media="screen" href="{{ asset('frontend/css/custom-style.css') }}">
-  <link rel="stylesheet" media="screen" href="{{ asset('frontend/css/ijaboCropTool.min.css') }}">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" type="text/javascript"></script>
-  <link rel="stylesheet" href="{{ asset('frontend/vendor/boxed/css/boxed-check.min.css') }}">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
-    integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
-    integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="{{ asset('frontend/vendor/boxed/css/boxed-check.min.css') }}">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/typeahead.bundle.min.js"></script>
-  <link rel="stylesheet" href="{{ URL::to('frontend/dist/assets/owl.carousel.min.css') }}">
-  <link rel="stylesheet" href="{{ URL::to('frontend/dist/assets/owl.theme.default.min.css') }}">
-  <link rel="stylesheet" href="{{ URL::to('frontend/magiczoom/magiczoom.css') }}">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
-    integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/typeahead.bundle.min.js"></script>
-  <script src="{{ asset('frontend/js/search.js') }}"></script>
-  <script src="{{ asset('frontend/js/wishlist.js') }}"></script>
-  <script src="{{ asset('frontend/js/account.js') }}"></script>
-  <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
-  <script src="{{ asset('frontend/js/chatbox.js') }}"></script>
-  @livewireStyles
-  @livewireScripts
-  @routes
+    <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>BigDeal | @yield('title')</title>
+    <!-- SEO Meta Tags-->
+    @yield('meta')
+    <!-- Viewport-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon and Touch Icons-->
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('frontend/img/logo/favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('frontend/img/logo/favicon.png') }}">
+    {{-- <link rel="manifest" href="site.webmanifest"> --}}
+    <link rel="mask-icon" color="#fe6a6a" href="safari-pinned-tab.svg">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">
+    <!-- Vendor Styles including: Font Icons, Plugins, etc.-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link rel="stylesheet" media="screen" href="{{ asset('frontend/vendor/simplebar/dist/simplebar.min.css') }}" />
+    <link rel="stylesheet" media="screen" href="{{ asset('frontend/vendor/tiny-slider/dist/tiny-slider.css') }}" />
+    <link rel="stylesheet" media="screen" href="{{ asset('frontend/vendor/drift-zoom/dist/drift-basic.min.css') }}" />
+    <link rel="stylesheet" media="screen"
+        href="{{ asset('frontend/vendor/lightgallery.js/dist/css/lightgallery.min.css') }}" />
+    <!-- Main Theme Styles + Bootstrap-->
+    <link rel="stylesheet" media="screen" href="{{ asset('frontend/css/theme.min.css') }}">
+    <link rel="stylesheet" media="screen" href="{{ asset('frontend/css/custom-style.css') }}">
+    <link rel="stylesheet" media="screen" href="{{ asset('frontend/css/ijaboCropTool.min.css') }}">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="{{ asset('frontend/vendor/boxed/css/boxed-check.min.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
+        integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="{{ asset('frontend/vendor/boxed/css/boxed-check.min.css') }}">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/typeahead.bundle.min.js"></script>
+    <link rel="stylesheet" href="{{ URL::to('frontend/dist/assets/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::to('frontend/dist/assets/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::to('frontend/magiczoom/magiczoom.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
+        integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/typeahead.bundle.min.js"></script>
+    <script src="{{ asset('frontend/js/search.js') }}"></script>
+    <script src="{{ asset('frontend/js/wishlist.js') }}"></script>
+    <script src="{{ asset('frontend/js/account.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
+    <script src="{{ asset('frontend/js/chatbox.js') }}"></script>
+    @livewireStyles
+    @livewireScripts
+    @routes
 </head>
 <!-- Body-->
 <script>
@@ -90,84 +87,84 @@
 </script>
 
 <body class="handheld-toolbar-enabled">
-  <!-- Sign in / sign up modal-->
-  @include('clients.Inc.modal-login')
-  <main class="page-wrapper">
-    <!-- Quick View Modal-->
-    @include('clients.Inc.quickview')
-    <!-- Navbar Electronics Store-->
-    @include('clients.Inc.header')
+    <!-- Sign in / sign up modal-->
+    @include('clients.Inc.modal-login')
+    <main class="page-wrapper">
+        <!-- Quick View Modal-->
+        @include('clients.Inc.quickview')
+        <!-- Navbar Electronics Store-->
+        @include('clients.Inc.header')
 
 
-    @yield('content')
-  </main>
-  <input type="hidden" name="" id="url_to" value="{{ URL::to('/') }}">
-  <!-- Footer-->
-  @include('clients.Inc.footer')
-  <!-- Toolbar for handheld devices (Default)-->
-  <div class="handheld-toolbar">
-    <div class="d-table table-layout-fixed w-100"><a class="d-table-cell handheld-toolbar-item"
-        href="account-wishlist.html"><span class="handheld-toolbar-icon"><i class="ci-heart"></i></span><span
-          class="handheld-toolbar-label">Yêu thích</span></a><a class="d-table-cell handheld-toolbar-item"
-        href="javascript:void(0)" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-        onclick="window.scrollTo(0, 0)"><span class="handheld-toolbar-icon"><i class="ci-menu"></i></span><span
-          class="handheld-toolbar-label">Menu</span></a><a class="d-table-cell handheld-toolbar-item"
-        href="shop-cart.html"><span class="handheld-toolbar-icon"><i class="ci-cart"></i><span
-            class="badge bg-primary rounded-pill ms-1">4</span></span><span
-          class="handheld-toolbar-label">$265.00</span></a></div>
-  </div>
-  {{-- Chatbox button --}}
-  @include('clients.Inc.chatbox')
-  <!-- GetButton.io widget -->
+        @yield('content')
+    </main>
+    <input type="hidden" name="" id="url_to" value="{{ URL::to('/') }}">
+    <!-- Footer-->
+    @include('clients.Inc.footer')
+    <!-- Toolbar for handheld devices (Default)-->
+    <div class="handheld-toolbar">
+        <div class="d-table table-layout-fixed w-100"><a class="d-table-cell handheld-toolbar-item"
+                href="account-wishlist.html"><span class="handheld-toolbar-icon"><i
+                        class="ci-heart"></i></span><span class="handheld-toolbar-label">Yêu thích</span></a><a
+                class="d-table-cell handheld-toolbar-item" href="javascript:void(0)" data-bs-toggle="collapse"
+                data-bs-target="#navbarCollapse" onclick="window.scrollTo(0, 0)"><span class="handheld-toolbar-icon"><i
+                        class="ci-menu"></i></span><span class="handheld-toolbar-label">Menu</span></a><a
+                class="d-table-cell handheld-toolbar-item" href="shop-cart.html"><span class="handheld-toolbar-icon"><i
+                        class="ci-cart"></i><span class="badge bg-primary rounded-pill ms-1">4</span></span><span
+                    class="handheld-toolbar-label">$265.00</span></a></div>
+    </div>
+    {{-- Chatbox button --}}
+    @include('clients.Inc.chatbox')
+    <!-- GetButton.io widget -->
 
-  <!-- /GetButton.io widget -->
-  <!-- Back To Top Button-->
-  <a class="btn-scroll-top" href="#top" data-scroll>
-    <span class="btn-scroll-top-tooltip text-muted fs-sm me-2">Top</span>
-    <i class="btn-scroll-top-icon ci-arrow-up"></i>
-  </a>
-  <!-- Vendor scrits: js libraries and plugins-->
-  <script src="{{ URL::to('backend/js/jquery-3.5.1.min.js') }}"></script>
-  <script src="{{ asset('fancybox/dist/jquery.fancybox.min.js') }}"></script>
-  <script src="{{ asset('frontend/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('frontend/vendor/simplebar/dist/simplebar.min.js') }}"></script>
-  <script src="{{ asset('frontend/vendor/tiny-slider/dist/min/tiny-slider.js') }}"></script>
-  <script src="{{ asset('frontend/vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js') }}"></script>
-  <script src="{{ asset('frontend/vendor/drift-zoom/dist/Drift.min.js') }}"></script>
-  <script src="{{ asset('frontend/vendor/lightgallery.js/dist/js/lightgallery.min.js') }}"></script>
-  <script src="{{ asset('frontend/vendor/lg-video.js/dist/lg-video.min.js') }}"></script>
-  <script type="text/javascript" src="https://cdn.rawgit.com/igorlino/elevatezoom-plus/1.1.6/src/jquery.ez-plus.js">
-  </script>
-  <!-- Main theme script-->
-  <script src="{{ asset('frontend/js/theme.min.js') }}"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/simplePagination.js/1.4/jquery.simplePagination.min.js">
-  </script>
-  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <script src="https://cdn.lordicon.com/libs/mssddfmo/lord-icon-2.1.0.js"></script>
-  <script src="http://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
-  <script src="{{ URL::to('frontend/js/cart.js') }}"></script>
-  <script src="{{ URL::to('frontend/dist/owl.carousel.min.js') }}"></script>
-  <script src="{{ URL::to('frontend/magiczoom/magiczoom.js') }}"></script>
-  <script src="{{ asset('frontend/js/ijaboCropTool.min.js') }}"></script>
-  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
-  <script src="{{ URL::to('frontend/js/cart.js') }}"></script>
-  <script src="{{ asset('frontend/js/ijaboCropTool.min.js') }}"></script>
-  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
-  <script src="{{ asset('frontend/js/checkout.js') }}"></script>
-  <!-- Include the PayPal JavaScript SDK -->
-  <script src="https://www.paypal.com/sdk/js?client-id=test&currency=USD" data-namespace="paypal_sdk"></script>
-  <script src="{{ asset('frontend/js/paypal.js') }}"></script>
-  @include('sweetalert::alert')
-  @stack('script')
-  <script type="text/javascript">
-    cartDropdown()
-    window.addEventListener('alert', event => {
-      toastr[event.detail.type](event.detail.message,
-        event.detail.title ?? ''), toastr.options = {
-        "closeButton": true,
-        "progressBar": true,
-      };
-    })
+    <!-- /GetButton.io widget -->
+    <!-- Back To Top Button-->
+    <a class="btn-scroll-top" href="#top" data-scroll>
+        <span class="btn-scroll-top-tooltip text-muted fs-sm me-2">Top</span>
+        <i class="btn-scroll-top-icon ci-arrow-up"></i>
+    </a>
+    <!-- Vendor scrits: js libraries and plugins-->
+    <script src="{{ URL::to('backend/js/jquery-3.5.1.min.js') }}"></script>
+    <script src="{{ asset('fancybox/dist/jquery.fancybox.min.js') }}"></script>
+    <script src="{{ asset('frontend/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('frontend/vendor/simplebar/dist/simplebar.min.js') }}"></script>
+    <script src="{{ asset('frontend/vendor/tiny-slider/dist/min/tiny-slider.js') }}"></script>
+    <script src="{{ asset('frontend/vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js') }}"></script>
+    <script src="{{ asset('frontend/vendor/drift-zoom/dist/Drift.min.js') }}"></script>
+    <script src="{{ asset('frontend/vendor/lightgallery.js/dist/js/lightgallery.min.js') }}"></script>
+    <script src="{{ asset('frontend/vendor/lg-video.js/dist/lg-video.min.js') }}"></script>
+    <script type="text/javascript" src="https://cdn.rawgit.com/igorlino/elevatezoom-plus/1.1.6/src/jquery.ez-plus.js">
+    </script>
+    <!-- Main theme script-->
+    <script src="{{ asset('frontend/js/theme.min.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/simplePagination.js/1.4/jquery.simplePagination.min.js">
+    </script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.lordicon.com/libs/mssddfmo/lord-icon-2.1.0.js"></script>
+    <script src="http://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
+    <script src="{{ URL::to('frontend/js/cart.js') }}"></script>
+    <script src="{{ URL::to('frontend/dist/owl.carousel.min.js') }}"></script>
+    <script src="{{ URL::to('frontend/magiczoom/magiczoom.js') }}"></script>
+    <script src="{{ asset('frontend/js/ijaboCropTool.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
+    <script src="{{ URL::to('frontend/js/cart.js') }}"></script>
+    <script src="{{ asset('frontend/js/ijaboCropTool.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
+    <script src="{{ asset('frontend/js/checkout.js') }}"></script>
+    <!-- Include the PayPal JavaScript SDK -->
+    <script src="https://www.paypal.com/sdk/js?client-id=test&currency=USD" data-namespace="paypal_sdk"></script>
+    <script src="{{ asset('frontend/js/paypal.js') }}"></script>
+    @include('sweetalert::alert')
+    @stack('script')
+    <script type="text/javascript">
+        cartDropdown()
+        window.addEventListener('alert', event => {
+            toastr[event.detail.type](event.detail.message,
+                event.detail.title ?? ''), toastr.options = {
+                "closeButton": true,
+                "progressBar": true,
+            };
+        })
 
         $(document).on('click', '#change_avatar', function() {
             $('#customer_avatar').click();
