ba6334fa156c7d52693ba3a8064561495138c234
cookie_cookie98 <83325511+cookiecookie98@users.noreply.github.com>
2021-12-10 08:48:18 +0700

    paypal

--- resources/views/clients/checkout/checkout-payment.blade.php
+++ resources/views/clients/checkout/checkout-payment.blade.php
@@ -122,30 +122,6 @@
 
 @push('script')
 <script>
-  // Render the PayPal button into #paypal-button-container
-    paypal.Buttons({
-
-      // Set up the transaction
-      createOrder: function(data, actions) {
-          return actions.order.create({
-              purchase_units: [{
-                  amount: {
-                      value: '1.00'
-                  }
-              }]
-          });
-      },
-
-      // Finalize the transaction
-      onApprove: function(data, actions) {
-          return actions.order.capture().then(function(orderData) {
-              console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
-              var transaction = orderData.purchase_units[0].payments.captures[0];
-              alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
-          });
-      }
-
-
-    }).render('#paypal-button-container');  
+  paypalButtons()
 </script>
 @endpush
