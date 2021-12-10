e0ca7029fdcea6c96cfa2298b2888411da233ed2
cookie_cookie98 <83325511+cookiecookie98@users.noreply.github.com>
2021-12-10 09:00:18 +0700

    paypal (Kich hoat buton)

--- resources/views/clients/checkout/checkout-details.blade.php
+++ resources/views/clients/checkout/checkout-details.blade.php
@@ -105,6 +105,37 @@
 
 @push('script')
 <script>
+  paypal.Buttons({
+
+            // Set up the transaction
+            createOrder: function(data, actions) {
+                return actions.order.create({
+                    purchase_units: [{
+                        amount: {
+                            value: '88.44'
+                        }
+                    }]
+                });
+            },
+
+            // Finalize the transaction
+            onApprove: function(data, actions) {
+                return actions.order.capture().then(function(orderData) {
+                    // Successful capture! For demo purposes:
+                    console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+                    var transaction = orderData.purchase_units[0].payments.captures[0];
+                    alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+
+                    // Replace the above to show a success message within this page, e.g.
+                    // const element = document.getElementById('paypal-button-container');
+                    // element.innerHTML = '';
+                    // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+                    // Or go to another URL:  actions.redirect('thank_you.html');
+                });
+            }
+
+
+            }).render('#paypal-button-container');
   $('#form-infomations').on('submit', function(event){
     event.preventDefault()
     let array = [];
